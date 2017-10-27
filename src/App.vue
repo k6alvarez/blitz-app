98c9f35e1ccea54dc65bd1e2f6bab82fd1a5cb4c
k6alvarez <k6alvarez@gmail.com>
2017-10-27 16:55:45 -0400

    start files

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view></router-view>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppFooter from './components/AppFooter'
+
+export default {
+  name: 'app',
+  components: { AppFooter }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+a, a:hover, a:visited {
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  padding: 0.6rem;
+}
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul class="footer">
+    <li>
+      <router-link to="/events">
+        <div>
+          <i class="fa fa-flag" aria-hidden="true">&nbsp;Team</i>
+        </div>
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/events">
+        <div>
+          <i class="fa fa-calendar" aria-hidden="true">&nbsp;Events</i>
+        </div>
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/events">
+        <div>
+          <i class="fa fa-user" aria-hidden="true">&nbsp;Profile</i>
+        </div>
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/events">
+        <div>
+          <i class="fa fa-gears" aria-hidden="true">&nbsp;Settings</i>
+        </div>
+      </router-link>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'app-footer'
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+
+i {
+  display: flex;
+  flex-direction: column;
+}
+</style>
